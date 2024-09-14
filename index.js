ef256473e2426c56f57b868e12a6bb0f680182c6
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-09-15 04:54:38 +0600

    menu delete

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT||5000;
 app.use(cors());
 app.use(express.json())
  
 
-console.log(process.env.DB_USER);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@atlascluster.6gwdl3v.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster;`
 
@@ -27,6 +26,23 @@ async function run() {
 //     await client.connect();
     const db = client.db('simplefood')
     const usersCollections = db.collection('users')
+    const menusCollections = db.collection('menu')
+    app.get('/menus',async(req,res)=>{
+       const result = await menusCollections.find().toArray()
+       res.send(result)
+    })
+    app.post('/menus',async(req,res)=>{
+     const body = req.body;
+      const result = await menusCollections.insertOne(body)
+      res.send(result)
+   })
+   app.delete('/menuDelete/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await menusCollections.deleteOne(query)
+        res.send(result)
+   })
+     
     app.get('/users',async(req,res)=>{
        const result = await usersCollections.find().toArray()
        res.send(result)
